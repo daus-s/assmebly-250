2b43641ba0e7675184b63b0ad26a87186377a8af
daus c <48344654+daus-s@users.noreply.github.com>
2019-03-09 22:55:58 -0800

    Add files via upload

--- sum.asm
+++ sum.asm
@@ -11,26 +11,33 @@
 			syscall					#wait for input
 			jr $ra					#return
 
+		#checks whether it is still OK to add n-value
 		check:
-			blez $s0, loop
+			bgtz $s0, loop
 			jr $ra
 
+		#loops through and does add components and increment
 		loop:
 			add $t1, $t1, $s0  #sum+=index
 			sub $s0, $s0, 1 #index--
 			jal check
 
+		#essentially i built system.out.print(int) but not really
 		output:
+			li $a0, 4
 			move $v0, $t1
-			li $v0, 4
+			syscall
+
+			li, $v0 10			#exit
+			syscall
+
 
 .globl main
 	main:
 	 	jal get_integer	#calling the method
-		move $s0, $v0		#moves value received to stored register
+	 	move $s0, $v0		#moves value received to stored register
 		jal check 			#contains loop
 		jal output			#print (n+1)n/2 but not actually cuz the lööp
-		li $v0, 10			#exit
 
 
 	.data
